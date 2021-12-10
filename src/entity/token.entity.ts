a391656ecccaeb75df027d81e8ab2f191000f488
Dmitriy <verbindima22@gmail.com>
2021-12-10 10:11:36 +0300

    feat: add token.entity and logic for tokenService

---
+++ src/entity/token.entity.ts
@@ -0,0 +1,29 @@
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Unique, ManyToOne, JoinColumn } from 'typeorm';
+import  { User }  from './user.entity';
+
+@Entity()
+export class Token extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(type => User, {
+        nullable: false
+    })
+
+    @JoinColumn({
+        name: 'userId'
+    })
+    user: User;
+
+    @Column({
+        type: 'varchar',
+        name: 'refresh_token'
+    })
+    refreshToken: string;
+
+    @Column({
+        type: 'timestamp',
+        default: new Date()
+    })
+    created: Date;
+}
---
+++ src/services/tokenService.ts
@@ -0,0 +1,66 @@
+import { Token } from '../entity/token.entity'
+import jwt from 'jsonwebtoken'
+import config from '../config/config'
+import { User } from 'entity/user.entity'
+class TokenService {
+  generateTokens(id:number, isAdmin:boolean) {
+    const payload = {
+      id,
+      isAdmin,
+    }
+    const accessToken = jwt.sign(payload, config.jwtAccessKey, {
+      expiresIn: '1h',
+    })
+    const refreshToken = jwt.sign(payload, config.jwtRefreshKey, {
+      expiresIn: '30h',
+    })
+    return {
+      accessToken,
+      refreshToken,
+    }
+  }
+  validateAccessToken(token:string) {
+    try {
+      const userData = jwt.verify(token, config.jwtAccessKey)
+      return userData
+    } catch (e) {
+      return null
+    }
+  }
+  validateRefreshToken(token:string) {
+    try {
+      const userData = jwt.verify(token, config.jwtRefreshKey)
+      return userData
+    } catch (e) {
+      return null
+    }
+  }
+  
+  async saveToken(user_Id:number, refreshToken:string):Promise<any> {
+    const tokenData = await Token.findOne({ where: { user: user_Id } })
+    //const tokenData = await Token.findOne({  })
+    console.log(tokenData)
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken
+      return tokenData.save()
+    }    
+    const token = Token.create({
+        refreshToken,
+        user: {
+            id: user_Id
+            }
+    })
+    console.log(token)
+    await token.save()
+    return token
+  }
+  async removeToken(refreshToken:string) {
+    const tokenData = await Token.delete({ refreshToken })
+    return tokenData
+  }
+  async findToken(refreshToken:string) {
+    const tokenData = await Token.findOne({ refreshToken })
+    return tokenData
+  }
+}
+export default new TokenService()
