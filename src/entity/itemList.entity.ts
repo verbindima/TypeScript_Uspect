76799fd5c9531d9ff6a7c9195aa95e2113c9efd5
Dmitriy <verbindima22@gmail.com>
2022-01-16 16:30:43 +0300

    feat: add entity itemList

---
+++ src/entity/itemList.entity.ts
@@ -0,0 +1,33 @@
+import {Entity, Column, JoinColumn, BaseEntity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Catalog } from './catalog.entity';
+import { Order } from './order.entity';
+@Entity()
+export class ItemList extends BaseEntity{
+
+    @PrimaryGeneratedColumn({ type: 'bigint' })
+    id: number;
+
+    
+
+    @ManyToOne(() => Order, {
+        nullable: true
+    })
+    @JoinColumn({
+        name: 'orderId'
+    })
+    order: Order;
+    
+    
+
+    @ManyToOne(() => Catalog, {
+        nullable:true
+    })
+    @JoinColumn({
+        name: 'catalogId'
+    })
+    catalog: Catalog;
+
+    @Column()
+    count: number;
+
+}
