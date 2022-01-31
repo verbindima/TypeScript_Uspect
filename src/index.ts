d77c59cecfb0e2318d56cec1a177a7802860a0fc
Dmitriy <verbindima22@gmail.com>
2022-01-31 20:18:35 +0300

    feat: add swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
 import express, { json } from 'express'
 import cookieParser from 'cookie-parser'
 import BodyParser from 'body-parser'
-import usersRouter from './routes/usersRouter'
-import catalogRouter from './routes/catalogRouter'
-import pizzeriasRouter from './routes/pizzeriaRouter'
-import ordersRouter from './routes/ordersRouter'
-import * as config from './config/config'
+import usersRouter from './api/routes/usersRouter'
+import catalogRouter from './api/routes/catalogRouter'
+import pizzeriasRouter from './api/routes/pizzeriaRouter'
+import ordersRouter from './api/routes/ordersRouter'
+import config from './config/config'
 import path from 'path'
-import { errors } from "celebrate";
-import errorMiddleware from "./middlewares/errorMiddleware";
+import { errors } from 'celebrate'
+import errorMiddleware from './api/middlewares/errorMiddleware'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerFile from './swagger.json'
 
-const PORT = config.default.PORT
+const PORT = config.PORT
 const app = express()
 app.use(json())
 app.use(cookieParser())
@@ -23,13 +25,15 @@ app.use('/api/users', usersRouter)
 app.use('/api/catalog', catalogRouter)
 app.use('/api/pizzerias', pizzeriasRouter)
 app.use('/api/orders', ordersRouter)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 app.use(errors())
 app.use(errorMiddleware)
 
 createConnection()
     .then(async () => {
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
-    
-}).catch(error => console.log(error));
+    })
+    .catch((error) => console.log(error))
 
 export default app
